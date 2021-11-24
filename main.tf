6bd6a7f48789f370be5389072d73ad614799b102
abhishektiwari <tiwarabhishek@google.com>
2021-11-24 11:43:49 +0530

    updated

--- main.tf
+++ main.tf
@@ -1,22 +1,48 @@
-resource "google_compute_firewall" "default" {
-  name    = "te-dev-fire-syst-firewall"
-  network = google_compute_network.default.name
-
-  allow {
-    protocol = "icmp"
-  }
-
-  allow {
-    protocol = "tcp"
-    ports    = ["443", "4433", "1000-2000"]
-  }
-
-  source_tags = ["web"]
-}
+#resource "google_compute_firewall" "default" {
+#  name    = "te-dev-fire-syst-firewall"
+#  network = google_compute_network.default.name
+#
+#  allow {
+#    protocol = "icmp"
+#  }
+#
+#  allow {
+#    protocol = "tcp"
+#    ports    = ["443", "4433", "1000-2000"]
+#  }
+#
+#  source_tags = ["web"]
+#}
 
 resource "google_compute_network" "default" {
   name = "te-dev-vpce-syst-testvpc"
   project                 = "my-project-name"
   auto_create_subnetworks = true
   mtu                     = 1500
+}
+
+module "firewall_rules" {
+  source       = "terraform-google-modules/network/google//modules/firewall-rules"
+  project_id   = var.project_id
+  network_name = google_compute_network.default.name
+
+  rules = [{
+    name                    = "allow-ssh-ingress"
+    description             = null
+    direction               = "INGRESS"
+    priority                = null
+    ranges                  = ["0.0.0.0/0"]
+    source_tags             = null
+    source_service_accounts = null
+    target_tags             = null
+    target_service_accounts = null
+    allow = [{
+      protocol = "tcp"
+      ports    = ["22"]
+    }]
+    deny = []
+    log_config = {
+      metadata = "INCLUDE_ALL_METADATA"
+    }
+  }]
 }
